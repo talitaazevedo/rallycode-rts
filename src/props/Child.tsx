1c9205bc8679ed796a6e3b1a8b434b6b89b74b62
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-15 19:35:06 -0300

    s02 - The Explicit Component Type Annotations

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -7,3 +7,5 @@ export const Child = ({ color }: ChildProps) => {
 
   return <div>{color}</div>
 }
+// * Here is another way to declare functional components I prefer That
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => { return <div>{color}</div> }
