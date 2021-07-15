de769471f5372a7cd1969add89b4926bfbd76766
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-15 20:27:19 -0300

    s02 - Type inference with State and Quick Finish to the example

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useState } from "react";
+
+
+
 const GuestList:React.FC = () => {
   const [ name,setName] = useState('');
+  const[guests,setGuests] = useState<string[]>([]);
+
+
+  const onClick = () =>{
+    setName('');
+    setGuests([...guests,name]);
+
+  };
   return <div>
     <h3>Guest List</h3>
+    <ul>
+      {guests.map(guest=><li key={guest}>{guest}</li>)}
+    </ul>
     <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
-    <button>Add Guest</button>
+    <button onClick ={onClick}>Add Guest</button>
     </div>
 };
 
